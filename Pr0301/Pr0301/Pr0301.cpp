263531e63131349759bb0b88162f8542c79627df
Yura1Chuyko <ura5509@gmail.com>
2020-10-19 14:49:19 +0300

    Add files via upload

---
+++ Pr0301/Pr0301/Pr0301.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>  
+using namespace std;
+int main()
+{
+    double x;
+    // вхідний параметр     
+    double y;
+    // результат обчислення виразу     
+    double A;  // проміжний результат - функціонально стала частина виразу     
+    double B;  // проміжний результат - функціонально змінна частина виразу  
+    cout << "x = ";
+    cin >> x;          
+    A = 2+ 1./abs(1+x);
+    // спосіб 1: розгалуження в скороченій формі     
+    if (x<-1)         
+    B = sqrt(abs(cos(x)+13));
+    if (1<=x && x<=5)
+        B =3+cos(4 + x / sqrt(2))/sin(4+x/sqrt(2)) ;
+    if (x>5)
+        B = 8+0.7*x;  
+    y = A + B;
+    cout << endl;
+    cout << "1) y = " << y << endl;
+    // спосіб 2: розгалуження в повній формі     
+    if (x < -1)
+        B = sqrt(abs(cos(x) + 13));
+    if (1 <= x && x <= 5)
+        B = 3 + cos(4 + x / sqrt(2)) / sin(4 + x / sqrt(2));
+    if (x > 5)
+        B = 8 + 0.7 * x;
+    y = A + B;
+    cout << "2) y = " << y << endl;
+    cin.get();
+    return 0;
+}
+
+
